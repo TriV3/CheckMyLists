e78eacdbca1b1cfa773e2b0d60cdc03870e437a5
Tristan VEYET <tristan.veyet@gmail.com>
2018-03-19 08:36:05 +0100

    NativeScript - Change for default project template

--- check-ns/app/app.module.ts
+++ check-ns/app/app.module.ts
@@ -1,9 +1,17 @@
-import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NSModuleFactoryLoader } from "nativescript-angular/router";
-
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
+
+import { ItemService } from "./item/item.service";
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
+// import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 @NgModule({
     bootstrap: [
@@ -14,13 +22,18 @@ import { AppComponent } from "./app.component";
         AppRoutingModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        ItemsComponent,
+        ItemDetailComponent
     ],
     providers: [
-        { provide: NgModuleFactoryLoader, useClass: NSModuleFactoryLoader }
+        ItemService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
 export class AppModule { }
